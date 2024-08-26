d6e1864a781824697ec3022afa233876712b8f06
MikeYang <mikeyoung427@gmail.com>
2024-08-26 23:04:41 +0800

    LED thread contorl by SPDT button

---
+++ led_spdt.cpp
@@ -0,0 +1,161 @@
+// ---------------------------------------------
+// Thread stop/ restart control by SPDT button
+// ---------------------------------------------
+#include <stdio.h>
+#include "wiringPi.h"
+#include "pthread.h"
+#include <iostream>
+using namespace std;
+
+int range = 100; // random range [0, 100]
+#define LED1_PIN 7
+#define LED2_PIN 0
+#define SWITCH_PIN 29
+
+int TURN = 0; // lock
+int FLAG[2];
+int STOP = false; // let thread wait
+int KILL = false; // stop thread
+pthread_t thread1, thread2;
+
+void *led1(void* argv);
+void *led2(void* argv);
+
+void init_LED()
+{
+    wiringPiSetup();
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+    pinMode(SWITCH_PIN, INPUT);
+}
+
+void *led1(void* argv)
+{
+    while(1){
+        /* kill thread */
+        if(KILL){
+            if(TURN==0) TURN=1;
+            printf("led1 thread stop.....\n");
+            break;
+        }
+        /* wait signal */
+        while(STOP){
+            printf("led1 thread waiting.....\n");
+            if(TURN==0) TURN=1;
+            delay(500);
+        }
+        /* acquire lock */
+        FLAG[0]=true;
+        TURN = 1;
+        while(FLAG[0] && TURN==1);
+        /* CS */
+        srand(time(NULL));
+        int rand_num = rand()%range + 0;
+        if(rand_num%7==0){
+            printf("[led1 on!!] | num: %d\n", rand_num);
+            digitalWrite(LED1_PIN, HIGH);
+            delay(1000);
+            digitalWrite(LED1_PIN, LOW);
+            delay(1000);
+            printf("------------------\n");
+        }
+        else{
+           printf("led1 off...\n");
+        }
+        /* release lock*/
+        FLAG[0] = false;
+        delay(500);
+    }
+    return NULL;
+}
+
+
+void *led2(void* argv)
+{
+    while(1){
+        /* kill thread */
+        if(KILL){
+            if(TURN==1) TURN =0;
+            printf("led2 thread stop...\n");
+            break;
+        }
+        /* wait signal */
+        while(STOP){
+            printf("led2 thread waiting.....\n");
+            if(TURN==1) TURN=0;
+            delay(500);
+        }
+        /* acquire lock */
+        FLAG[1]=true;
+        TURN = 0;
+        while(FLAG[1] && TURN==0);
+        /* CS */
+        srand(time(NULL));
+        int rand_num = rand()%range + 0;
+        if(rand_num%9==0){
+            printf("[led2 on!!] | num: %d\n", rand_num);
+            digitalWrite(LED2_PIN, HIGH);
+            delay(1000);
+            digitalWrite(LED2_PIN, LOW);
+            delay(1000);
+            printf("------------------\n");
+        }
+        else{
+           printf("led2 off...\n");
+        }
+        /* release lock */
+        FLAG[1] = false;
+        delay(500);
+    }
+    return NULL;
+}
+
+void clear_handle()
+{
+    KILL=true;
+    digitalWrite(LED1_PIN, LOW);
+    digitalWrite(LED2_PIN, LOW);
+    pthread_join(thread1, NULL);
+    delay(500);
+    pthread_join(thread2, NULL);
+    delay(500);
+    printf("All thread kill...\n");
+    return;
+}
+
+int main(void)
+{
+
+    init_LED();
+
+    int iret1 = pthread_create(&thread1, NULL, led1, NULL);
+    if(iret1 != 0){
+	    fprintf(stderr, "pthread_create thread1 failed!");
+	    return 0;
+    }
+    int iret2 = pthread_create(&thread2, NULL, led2, NULL);
+    if(iret2 != 0){
+	    fprintf(stderr, "pthread_create thread2 failed!");
+	    return 0;
+    }
+    printf("create led threads\n");
+    
+    int open = false; // switch button status
+    /* detect signal to change thread status */
+    while(1){
+        /* SPDT button */
+        if(digitalRead(SWITCH_PIN)==HIGH){
+            printf("button pressed...\n");
+            open ^= 1;
+            printf("gate status: %d\n", open);
+            if(open){
+                digitalWrite(LED1_PIN, LOW);
+                digitalWrite(LED2_PIN, LOW);
+                STOP=true;
+            }
+            else STOP=false;
+        }
+        delay(500);
+    }
+    return 0;
+}
